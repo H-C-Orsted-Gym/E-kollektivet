f2a6fbf2548b7713cc29182874dfae6c8eff0503
fredrikj31 <fredrik1206@gmail.com>
2020-03-12 09:44:25 +0100

    Added register user

--- includes/php/classes/users.inc.php
+++ includes/php/classes/users.inc.php
@@ -12,17 +12,53 @@
 				$count = $result->num_rows;
 				if ($count == 1) {
 					while ($row = $result->fetch_assoc()) {
-						$_SESSION
+						session_start();
+						$_SESSION['Id'] = $row['Id'];
+						$_SESSION['Username'] = $row['Username'];
+						$_SESSION['Email'] = $row['Email'];
+						$_SESSION['Firstname'] = $row['Firstname'];
+						$_SESSION['Lastname'] = $row['Lastname'];
+						$_SESSION['Birthday'] = $row['Birthday'];
+						$_SESSION['Room'] = $row['Room'];
+						$_SESSION['Role'] = $row['Role'];
+
+						return 200;
 					}
 				} else {
 					return 401;
 				}
-				
 			} else {
 				return 500;
 			}
-			
-			
+		}
+
+		public function checkSession() {
+			session_start();
+			if (!isset($_SESSION['Id'])) {
+				return 401;	
+			}
+		}
+
+		public function logout() {
+			session_start();
+
+			if (isset($_SESSION['Id'])) {
+				return 401;
+			} else {
+				session_destroy();
+			}
+		}
+
+		public function registerUser($Username, $Password, $Firstname, $Lastname, $Email, $CPR_Number, $Birthday, $Gender, $Room, $Role) {
+			$hashPassword = $this->generateHash($Password);
+
+			$sql = "INSERT INTO `users`(`Id`, `Username`, `Password`, `Firstname`, `Lastname`, `Email`, `CPR-Number`, `Birthday`, `Gender`, `Room`, `Role`) VALUES ('','$Username','$hashPassword','$Firstname','$Lastname','$Email','$CPR_Number','$Birthday','$Gender','$Room', '$Role')";
+			$result = $this->connect()->query($sql);
+			if ($result == TRUE) {
+				return 200;
+			} else {
+				return 500;
+			}
 		}
 
 
