faa658b46bd8df478a79a30453c95e543a9e84be
fredrikj31 <fredrik1206@gmail.com>
2020-03-18 09:22:38 +0100

    Test Push

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>E-Kollektivet - Dashboard</title>
+        <title>E-Kollektivet - Hjem</title>
         <link href="includes/css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
